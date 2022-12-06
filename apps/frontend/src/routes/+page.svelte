b2eff5a80e4204e7ed62d7feaaf655fabe150c20
Etienne <etienne.bouteille@gmail.com>
2022-12-06 22:38:23 +0100

    improved ui

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { isEmpty } from 'lodash';
-	import date from 'date-and-time';
+	// import date from 'date-and-time';
 	import { getWeek, getWorkedDaysInMonth } from '../helpers/getWeekOf';
 	import PocketBase from 'pocketbase';
 	const pb = new PocketBase('http://127.0.0.1:8090');
 	import startOfWeek from 'date-fns/startOfWeek';
 	import endOfWeek from 'date-fns/endOfWeek';
+	import addDays from 'date-fns/addDays';
 
 	export let data;
 	$: commutes = data.commutes;
@@ -14,6 +15,9 @@
 	$: currentWeek = getWeek(new Date(currentDay));
 	$: currentMonth = new Date(currentDay).getMonth();
 	let commuteLength = 23.6;
+	$: firstDayOfWeek = startOfWeek(currentDay, { weekStartsOn: 1 });
+
+	const shortDaysString = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven'];
 
 	const daysOfTheWeek = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi'];
 	$: weekdays = daysOfTheWeek.map((d) => {
@@ -33,7 +37,7 @@
 
 		const yr = new Date().getFullYear();
 		const payload = {
-			date: date.addDays(currentDay, dayDelta),
+			date: addDays(currentDay, dayDelta),
 			bike: 'en2el7ymwn7bl0j',
 			textfiled: 'test',
 			field: 'test'
@@ -60,41 +64,99 @@
 	});
 
 	$: workedDaysInMonth = getWorkedDaysInMonth(currentDay, currentMonth);
-	$: currentMonthStat = Math.round(selectedMonth.length / workedDaysInMonth);
+	$: currentMonthStat = Math.round((selectedMonth.length * 100) / workedDaysInMonth);
 	$: totalDistance = commutes.length * commuteLength;
 	$: weeklyDistance = Math.round(selectedWeek.length * commuteLength);
 </script>
 
 <div id="container" class="w-80 m-auto pt-5">
-	<h2 class="text-slate-500">Depuis le début</h2>
+	<h2 class="text-slate-500 text-xl">Depuis le début</h2>
 	<h1 class="mt-0 leading-none text-slate-900"><span>{Math.round(totalDistance)}</span>km</h1>
 
-	<div class="bg-slate-100 rounded-lg mt-5 pb-3 pt-3 flex justify-around">
+	<div class="bg-slate-100 rounded-xl mt-5 pb-3 pt-3 flex justify-evenly items-center">
 		<div class="text-center">
 			<p class="font-semibold text-slate-700">{currentMonthStat}%</p>
-			<p class="text-emerald-400 font-light text-sm">Ce mois</p>
+			<p class="text-emerald-400 font-light text-sm capitalize">
+				{new Date(currentDay).toLocaleDateString('fr-FR', { month: 'long' })}
+			</p>
 		</div>
+		<div style="border-left:1px solid #cbd5e1; height:30px; " />
 		<div class="text-center">
 			<p class="font-semibold text-slate-700">95%</p>
 			<p class="text-pink-500 font-light text-sm">Tendance</p>
 		</div>
+		<div style="border-left:1px solid #cbd5e1; height:30px; " />
 		<div class="text-center">
 			<p class="font-semibold text-slate-700">7.5kg</p>
 			<p class="text-amber-400 font-light text-sm">C02</p>
 		</div>
 	</div>
 
-	<h2>
-		{startOfWeek(currentDay, { weekStartsOn: 1 }).toLocaleDateString('fr-FR', {
-			day: 'numeric',
-			month: 'short'
-		})}
-		-
-		{endOfWeek(currentDay, { weekStartsOn: 1 }).toLocaleDateString('fr-FR', {
-			day: 'numeric',
-			month: 'short'
-		})}
-	</h2>
+	<div id="weektitle" class="flex justify-center items-center gap-10 mt-8">
+		<svg
+			class="fill-slate-700 cursor-pointer"
+			on:click={() => {
+				currentDay = addDays(currentDay, -7);
+			}}
+			on:keydown={() => {
+				currentDay = addDays(currentDay, -7);
+			}}
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 320 512"
+			><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+				d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+			/></svg
+		>
+		<h2 class="text-slate-700 text-xl  text-center">
+			{startOfWeek(currentDay, { weekStartsOn: 1 }).toLocaleDateString('fr-FR', {
+				day: 'numeric',
+				month: 'short'
+			})}
+			-
+			{endOfWeek(currentDay, { weekStartsOn: 1 }).toLocaleDateString('fr-FR', {
+				day: 'numeric',
+				month: 'short'
+			})}
+		</h2>
+		<svg
+			class="fill-slate-700 cursor-pointer"
+			on:click={() => {
+				currentDay = addDays(currentDay, +7);
+			}}
+			on:keydown={() => {
+				currentDay = addDays(currentDay, +7);
+			}}
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 320 512"
+			><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+				d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"
+			/></svg
+		>
+	</div>
+
+	<div class="bg-slate-100 rounded-xl flex justify-evenly mt-2 py-4">
+		{#each weekdays as day, index}
+			<div class="flex flex-col items-center gap-3">
+				<p>{shortDaysString[index]}</p>
+				<div
+					class="{!isEmpty(day)
+						? 'bg-emerald-400'
+						: 'bg-slate-300'} aspect-square rounded-full flex justify-center items-center cursor-pointer"
+					style="width: 42px;"
+					on:click={(e) => {
+						handleCheckedDate(index);
+					}}
+					on:keydown={(e) => {
+						handleCheckedDate(index);
+					}}
+				>
+					<p class="text-lg font-semibold text-lime-50">
+						{addDays(firstDayOfWeek, index).toLocaleDateString('fr-FR', { day: 'numeric' })}
+					</p>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <!-- <p>Distance semaine : {weeklyDistance}km</p>
@@ -117,7 +179,7 @@
 			type="button"
 			class="w-10 h-10 leading-10 text-gray-600 transition hover:opacity-75"
 			on:click={() => {
-				currentDay = date.addDays(currentDay, 7);
+				currentDay = addDays(currentDay, 7);
 			}}>&plus;</button
 		>
 		<span>
@@ -132,7 +194,7 @@
 			type="button"
 			class="w-10 h-10 leading-10 text-gray-600 transition hover:opacity-75"
 			on:click={() => {
-				currentDay = date.addDays(currentDay, -7);
+				currentDay = addDays(currentDay, -7);
 			}}>&minus;</button
 		>
 	</div>
@@ -172,5 +234,10 @@
 
 	h1 span {
 		font-size: 1.6em;
+	}
+
+	#weektitle svg {
+		max-height: 20px;
+		/* fill: #64748b; */
 	}
 </style>
