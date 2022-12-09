772a4635f97a3cd49e9c3775f2a52cf1142703be
Etienne Bouteillé <ebouteille@vinatis.com>
2022-12-09 13:18:06 +0100

    changing adapter back to auto for netlify

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,16 +1,16 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter: adapter()
-		adapter: adapter({
-			fallback: '200.html'
-		})
+		// adapter: adapter({
+		// 	fallback: '200.html'
+		// })
 	},
-	prerender: { entries: [] },
+	// prerender: { entries: [] },
 	preprocess: [
 		preprocess({
 			postcss: true
