65fabec1e817cd84553d8bb8cf4878d5003a4201
Etienne Bouteillé <ebouteille@vinatis.com>
2022-12-10 15:11:05 +0100

    pwa workds! adding icons

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -3,7 +3,30 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), SvelteKitPWA()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'flytta512.png'],
+			manifest: {
+				name: 'Flytta - mobilité durable',
+				short_name: 'Flytta',
+				description: 'Super app pour tracker ses déplacements à vélo',
+				theme_color: '#f0f6ee',
+				icons: [
+					{
+						src: 'android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'flytta512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
 };
 
 export default config;
