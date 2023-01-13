8927ba45ba92e7a48f9d8de609c3aa78c3cc3ade
Etienne Bouteillé <ebouteille@vinatis.com>
2023-01-13 14:35:12 +0100

    automatically loging in users after account creation

--- apps/frontend/src/routes/register/+page.server.js
+++ apps/frontend/src/routes/register/+page.server.js
@@ -18,6 +18,8 @@ export const actions = {
 		};
 		try {
 			await locals.pb.collection('users').create(userdata);
+			console.log('cration finished, connecting')
+			await locals.pb.collection('users').authWithPassword(body.username, body.password);
 		} catch (e) {
 			console.log('failed to create user : ', e);
 			throw Error(500, 'oops');
