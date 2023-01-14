732e9fc67eef818c4a98d16c1fddd7dbf79354c4
Etienne Bouteillé <ebouteille@vinatis.com>
2023-01-14 20:25:27 +0100

    trying to move to edge functions

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -6,7 +6,9 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge:true
+		})
 		// adapter: adapter({
 		// 	fallback: '200.html'
 		// })
